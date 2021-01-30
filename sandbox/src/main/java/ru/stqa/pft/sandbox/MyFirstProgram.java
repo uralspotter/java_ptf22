bc939f51311c747b7659e8d7e5131d2957f7ff33
egor.shishebarov <eshishebarov@lab-code.com>
2021-01-30 16:23:54 +0500

    Внесение изменений в классы Point и MyFirstProgram
    Функция disntance перенесена из Point в MyFirstProgram

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,12 +15,17 @@ public class MyFirstProgram {
 
         Point p1 = new Point(1,1);
         Point p2 = new Point(4, 1);
+        System.out.println("Расстояние между точкой p1 с координатой х=" + p1.x + " y=" + p1.y + " и p2 с координатами x=" + p2.x + " y=" + p2.y + " равно " + distance(p1, p2) + " рассчитанное функцией");
         System.out.println("Расстояние между точкой p1 с координатой х=" + p1.x + " y=" + p1.y + " и p2 с координатами x=" + p2.x + " y=" + p2.y + " равно " + p1.distanceMethod(p2) + " рассчитанное методом");
 
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
 
 }
