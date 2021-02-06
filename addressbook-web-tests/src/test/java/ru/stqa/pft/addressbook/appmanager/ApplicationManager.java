254db20d95a882b9b8de06a31e0b41fa63872259
egor.shishebarov <eshishebarov@lab-code.com>
2021-02-06 20:10:15 +0500

    Реализована двухуровневая архитектура тестового набора

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -26,24 +24,6 @@ public class ApplicationManager {
     public void stop() {
         wd.findElement(By.linkText("Logout")).click();
         wd.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            groupHelper.wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            groupHelper.wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 
     public GroupHelper getGroupHelper() {
