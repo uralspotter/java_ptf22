5b45d2931ad397c97d940dff400f7c32870804f8
egor.shishebarov <eshishebarov@lab-code.com>
2021-03-19 18:56:47 +0500

    Создан новый модуль в проект для тестирования BTS Mantis

---
+++ mantis-tests/src/test/Java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
